8bb777faa6d11e7adfd705a8949788744ecb929b
Damar Narváez Martínez <damar23nnm@gmail.com>
2022-08-26 05:39:01 -0500

    TiendaVirtual

---
+++ myTiendaVirtual/src/main/java/com/tiendavirtual/MyTiendaVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.tiendavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTiendaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTiendaVirtualApplication.class, args);
+	}
+
+}
