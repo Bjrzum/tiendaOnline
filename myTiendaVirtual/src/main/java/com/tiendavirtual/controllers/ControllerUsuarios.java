8452fe0a6c68714af986ae18547c781b856c40a1
Damar Narvaez <damar24nnm@gmail.com>
2022-09-01 21:57:32 -0500

    Agregando controladores

--- myTiendaVirtual/src/main/java/com/tiendavirtual/controllers/ControllerUsuarios.java
+++ myTiendaVirtual/src/main/java/com/tiendavirtual/controllers/ControllerUsuarios.java
@@ -1,2 +1,13 @@
-package com.tiendavirtual.controllers;public class ControllerUsuarios {
+package com.tiendavirtual.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ControllerUsuarios {
+    //Mapear la ruta para mostrar el listado
+    @GetMapping({"/usuarios"})
+    public String Listar(){
+        return "/usuarios/usuarios";
+    }
 }
